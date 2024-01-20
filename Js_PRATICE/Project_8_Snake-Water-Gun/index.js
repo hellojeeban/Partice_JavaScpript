71c2ecdc885a29abc29beb7008651efe7e893c01
Jeebanjyoti Biswal <76034296+hellojeeban@users.noreply.github.com>
2024-01-20 10:12:54 +0530

    new update

--- Js_PRATICE/Project_8_Snake-Water-Gun/index.js
+++ Js_PRATICE/Project_8_Snake-Water-Gun/index.js
@@ -1,7 +1,13 @@
 let choice;
 const my_arr = ['Snake', 'Water', 'Gun'];
 let randomElement;
+
+// let snake = 'img/snake.png';
+// let water = 'img/water.png';
+// let gun = 'img/gun.png';
+
 const res = document.getElementById("result");
+//const img = document.getElementById("img");
 
 function snake(){
     choice = document.getElementById("snake").value;
@@ -23,12 +29,16 @@ function random(){
 function start(){
     random();
     if(randomElement === choice){
+       // img.innerHTML = `<img src="${}" alt=""> `
         res.innerHTML = "Play Again";
     }else if(randomElement == 'Snake' && choice == 'Water'){
+       // img.innerHTML = `<img src="${snake}" alt=""> == <img src="${water}" alt="">`;
         res.innerHTML = `You Lose because my Choice is ${randomElement}`;
     }else if(randomElement == 'Water' && choice == 'Gun'){
+       // img.innerHTML = `<img src="${water}" alt=""> == <img src="${gun}" alt="">`;
         res.innerHTML = `You Lose because my Choice is ${randomElement}`;
     }else if(randomElement == 'Gun' && choice == 'Snake'){
+        //img.innerHTML = `<img src="${gun}" alt=""> == <img src="${snake}" alt="">`
         res.innerHTML = `You Lose because my Choice is ${randomElement}`;
     }else{
         res.innerHTML = `You Won because my Choice is ${randomElement}`;
