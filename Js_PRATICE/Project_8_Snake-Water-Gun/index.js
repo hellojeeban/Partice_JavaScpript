d6433fcb963f85f1c59d101494aba020288c7f12
Jeebanjyoti Biswal <76034296+hellojeeban@users.noreply.github.com>
2024-01-20 10:46:57 +0530

    refresh function added

--- Js_PRATICE/Project_8_Snake-Water-Gun/index.js
+++ Js_PRATICE/Project_8_Snake-Water-Gun/index.js
@@ -2,45 +2,80 @@ let choice;
 const my_arr = ['Snake', 'Water', 'Gun'];
 let randomElement;
 
-// let snake = 'img/snake.png';
-// let water = 'img/water.png';
-// let gun = 'img/gun.png';
+let snakeImg = 'img/snake.png';
+let waterImg = 'img/water.png';
+let gunImg = 'img/gun.png';
+
+let choiceImg;
 
 const res = document.getElementById("result");
 //const img = document.getElementById("img");
 
 function snake(){
     choice = document.getElementById("snake").value;
+    choiceImg = snakeImg;
     start();
 }
 function water(){
     choice = document.getElementById("water").value;
+    choiceImg= waterImg;
     start();
 }
 function gun(){
     choice = document.getElementById("gun").value;
+    choiceImg = gunImg;
     start();
 }
 
 function random(){
     randomElement = my_arr[Math.floor(Math.random() * my_arr.length)];
 }
+function create(src1, src2){
+    let img1 = document.createElement('img');
+    let img2 = document.createElement('img');
+    img1.src = src1;
+    img2.src = src2;
+    document.getElementById('img').appendChild(img1);
+    document.getElementById('img').appendChild(img2);
+}
+
+function autoRefresh() {
+    window.location = window.location.href;
+}
 
 function start(){
     random();
     if(randomElement === choice){
-       // img.innerHTML = `<img src="${}" alt=""> `
+        let img;
+        if(randomElement == 'Snake'){
+            img = snakeImg;
+        }else if(randomElement == 'Water'){
+            img = waterImg;
+        }else {
+            img = gunImg;
+        }
+        create(img, choiceImg);
         res.innerHTML = "Play Again";
     }else if(randomElement == 'Snake' && choice == 'Water'){
-       // img.innerHTML = `<img src="${snake}" alt=""> == <img src="${water}" alt="">`;
+        create(snakeImg, waterImg);
         res.innerHTML = `You Lose because my Choice is ${randomElement}`;
     }else if(randomElement == 'Water' && choice == 'Gun'){
-       // img.innerHTML = `<img src="${water}" alt=""> == <img src="${gun}" alt="">`;
+        create(waterImg, gunImg);
         res.innerHTML = `You Lose because my Choice is ${randomElement}`;
     }else if(randomElement == 'Gun' && choice == 'Snake'){
-        //img.innerHTML = `<img src="${gun}" alt=""> == <img src="${snake}" alt="">`
+        create(gunImg, snakeImg);
         res.innerHTML = `You Lose because my Choice is ${randomElement}`;
     }else{
+        let img;
+        if(randomElement == 'Snake'){
+            img = snakeImg;
+        }else if(randomElement == 'Water'){
+            img = waterImg;
+        }else {
+            img = gunImg;
+        }
+        create(img, choiceImg);
         res.innerHTML = `You Won because my Choice is ${randomElement}`;
     }
+    setInterval('autoRefresh()', 1200);
 }
