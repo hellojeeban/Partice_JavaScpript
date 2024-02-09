cd396b39926b612e8c921c32b45e5c5fc8f0cff6
Jeebanjyoti Biswal <76034296+hellojeeban@users.noreply.github.com>
2024-02-09 17:25:20 +0530

    new project

---
+++ Js_PRATICE/Project_9_idea/index.js
@@ -0,0 +1,11 @@
+const bulb = document.getElementById("image");
+
+function idea(){
+    bulb.src="idea.png";
+    document.body.style.backgroundColor="black";
+}
+
+function noidea(){
+    bulb.src="noidea.png";
+    document.body.style.backgroundColor="white";
+}
